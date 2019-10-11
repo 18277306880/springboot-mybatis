0efca351d737ceceecad94d13cfa287cad754192
machi <782349804@qq.com>
2019-10-11 10:06:29 +0800

    提交部署

---
+++ src/main/java/com/example/springbootmybatis/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.springbootmybatis.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public  String test(){
+        return  "测试自动化部署1";
+    }
+}
